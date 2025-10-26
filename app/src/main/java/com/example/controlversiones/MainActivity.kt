513066e3255f61e11ce5be4161c45cfd611224b1
emmderc <emmanuelmtzcr547@gmail.com>
2025-10-26 01:06:54 -0600

    Implementada la función para eliminar tareas del arreglo

--- app/src/main/java/com/example/controlversiones/MainActivity.kt
+++ app/src/main/java/com/example/controlversiones/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.example.controlversiones
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    inner class TaskAdapter(context: Context, private val tasks: ArrayList<String>) : ArrayAdapter<String>(context, 0, tasks) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var itemView = convertView
+            if (itemView == null) {
+                itemView = LayoutInflater.from(context).inflate(R.layout.list_item_task, parent, false)
+            }
+
+            val task = tasks[position]
+
+            val textViewTask = itemView!!.findViewById<TextView>(R.id.textViewTask)
+            val buttonDeleteTask = itemView.findViewById<Button>(R.id.buttonDeleteTask)
+
+            textViewTask.text = task
+
+            buttonDeleteTask.setOnClickListener {
+                tasks.removeAt(position)
+                notifyDataSetChanged()
+            }
+
+            return itemView
+        }
     }
 }
+
